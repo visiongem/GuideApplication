fcdc55d533e6af79155791e9c439c55cae4f722b
pengyanni <393507488@qq.com>
2018-03-15 14:28:34 +0800

    material design

---
+++ Locatr/src/main/java/com/pyn/locatr/FlickrFetchr.java
@@ -0,0 +1,23 @@
+package com.pyn.locatr;
+
+import android.location.Location;
+
+/**
+ * Created by pengyanni on 2018/3/14.
+ */
+
+public class FlickrFetchr {
+
+//    public List<GalleryItem> searchPhotos(Location location) {
+//        String url = buildUrl(location);
+//        return downloadGalleryItems(url);
+//    }
+//
+//    private String buildUrl(Location location) {
+//        return ENDPOINT.buildUpon()
+//                .appendQueryParameter("method", SEARCH_METHOD)
+//                .appendQueryParameter("lat", "" + location.getLatitude())
+//                .appendQueryParameter("lon", "" + location.getLongitude())
+//                .build().toString();
+//    }
+}
---
+++ Locatr/src/main/java/com/pyn/locatr/LocatrFragment.java
@@ -0,0 +1,95 @@
+package com.pyn.locatr;
+
+import android.Manifest;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.LocationSource;
+
+import java.util.Map;
+
+/**
+ * Created by pengyanni on 2018/3/13.
+ */
+public class LocatrFragment extends Fragment {
+
+    private static final String[] LOCATION_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+
+    private ImageView mImageView;
+    private GoogleApiClient mClient;
+
+    public static LocatrFragment newInstance() {
+        return new LocatrFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity())
+//                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                    }
+                })
+                .build();
+    }
+
+//    private void findImage() {
+//        LocationRequest request = LocationRequest.create();
+//        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//        request.setNumUpdates(1);
+//        request.setInterval(0);
+//    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        mClient.disconnect();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
+        mImageView = v.findViewById(R.id.image);
+        return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
+    }
+}
